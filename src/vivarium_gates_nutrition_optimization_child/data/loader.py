0852c7861a014794d0eef38f2aed469946fc7353
Patrick Nast <pnast@uw.edu>
2023-08-22 14:42:12 -0700

    big fixes

--- src/vivarium_gates_nutrition_optimization_child/data/loader.py
+++ src/vivarium_gates_nutrition_optimization_child/data/loader.py
@@ -199,8 +199,8 @@ def filter_population(unfiltered: pd.DataFrame) -> pd.DataFrame:
 
 
 def load_age_bins(key: str, location: str) -> pd.DataFrame:
-    all_age_bins = utilities.get_gbd_age_bins(metadata.AGE_GROUP.GBD_2021).set_index(['age_start', 'age_end', 'age_group_name']).sort_index()
-    return all_age_bins[all_age_bins.age_start < 5]
+    all_age_bins = utilities.get_gbd_age_bins(metadata.AGE_GROUP.GBD_2021)
+    return all_age_bins[all_age_bins.age_start < 5].set_index(['age_start', 'age_end', 'age_group_name']).sort_index()
 
 
 def load_demographic_dimensions(key: str, location: str) -> pd.DataFrame:
