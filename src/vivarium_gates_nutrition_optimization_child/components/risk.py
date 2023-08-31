4d64a6fd070a0d43b3107bc3536efcd6dc0e7162
hussain-jafari <hussainsjafari@gmail.com>
2023-08-31 13:49:21 -0500

    black and isort

--- src/vivarium_gates_nutrition_optimization_child/components/risk.py
+++ src/vivarium_gates_nutrition_optimization_child/components/risk.py
@@ -1,26 +1,28 @@
-import pandas as pd
-from vivarium_public_health.risks import Risk
-from vivarium.framework.engine import Builder
-from vivarium.framework.values import Pipeline
-
-from vivarium_public_health.risks.data_transformations import (
-    get_exposure_post_processor,
-)
-from vivarium_gates_nutrition_optimization_child.constants import data_keys, data_values
-from vivarium_public_health.risks.distributions import PolytomousDistribution
-from vivarium_public_health.risks.data_transformations import pivot_categorical
 import itertools
 from typing import Dict
 
+import pandas as pd
+from vivarium.framework.engine import Builder
+from vivarium.framework.values import Pipeline
+from vivarium_public_health.risks import Risk
+from vivarium_public_health.risks.data_transformations import (
+    get_exposure_post_processor,
+    pivot_categorical,
+)
+from vivarium_public_health.risks.distributions import PolytomousDistribution
+
+from vivarium_gates_nutrition_optimization_child.constants import data_keys, data_values
+
 
 class Underweight(Risk):
-    '''Model underweight risk in children. We model underweight using probability distributions
+    """Model underweight risk in children. We model underweight using probability distributions
     conditional on stunting and wasting exposure. Instead of using a standard exposure distribution,
     the expoure pipeline will determine which distribution to use separately for each joint stunting
-    and wasting state.'''
+    and wasting state."""
+
     def __init__(self):
-        super().__init__('risk_factor.underweight')
-        self._sub_components = [] # no exposure distribution
+        super().__init__("risk_factor.underweight")
+        self._sub_components = []  # no exposure distribution
 
     #################
     # Setup methods #
@@ -41,21 +43,28 @@ class Underweight(Risk):
             self.exposure_pipeline_name,
             source=self._get_current_exposure,
             requires_columns=["age", "sex"],
-            requires_values=[self.propensity_pipeline_name, data_values.PIPELINES.STUNTING_EXPOSURE, data_values.PIPELINES.WASTING_EXPOSURE],
+            requires_values=[
+                self.propensity_pipeline_name,
+                data_values.PIPELINES.STUNTING_EXPOSURE,
+                data_values.PIPELINES.WASTING_EXPOSURE,
+            ],
             preferred_post_processor=get_exposure_post_processor(builder, self.risk),
         )
 
     def _get_distributions(self, builder: Builder) -> Dict[str, PolytomousDistribution]:
-        '''Store and setup distributions for each joint stunting and wasting state.'''
+        """Store and setup distributions for each joint stunting and wasting state."""
         distributions = {}
         categories = [f"cat{i+1}" for i in range(4)]
         all_distribution_data = builder.data.load(data_keys.UNDERWEIGHT.EXPOSURE)
 
         for category_1, category_2 in itertools.product(categories, categories):
             key = f"stunting_{category_1}_wasting_{category_2}"
-            distribution_data = all_distribution_data.query("stunting_parameter == @category_1 and "
-                                                            "wasting_parameter == @category_2")
-            distribution_data = distribution_data.drop(['stunting_parameter', 'wasting_parameter'], axis=1)
+            distribution_data = all_distribution_data.query(
+                "stunting_parameter == @category_1 and " "wasting_parameter == @category_2"
+            )
+            distribution_data = distribution_data.drop(
+                ["stunting_parameter", "wasting_parameter"], axis=1
+            )
             distribution_data = pivot_categorical(distribution_data)
             distributions[key] = PolytomousDistribution(key, distribution_data)
         for dist in distributions.values():
@@ -67,18 +76,22 @@ class Underweight(Risk):
     ##################################
 
     def _get_current_exposure(self, index: pd.Index) -> pd.Series:
-        '''Calculate exposures separately for each joint stunting and wasting state and concatenate.'''
+        """Calculate exposures separately for each joint stunting and wasting state and concatenate."""
         if len(index) == 0:
-            return pd.Series(index=index) # only happens on first time step when there's no simulants
-        propensity = self.propensity(index).rename('propensity')
-        wasting = self.wasting(index).rename('wasting')
-        stunting = self.stunting(index).rename('stunting')
+            return pd.Series(
+                index=index
+            )  # only happens on first time step when there's no simulants
+        propensity = self.propensity(index).rename("propensity")
+        wasting = self.wasting(index).rename("wasting")
+        stunting = self.stunting(index).rename("stunting")
         pop = pd.concat([stunting, wasting, propensity], axis=1)
 
         exposures = []
-        for group, group_df in pop.groupby(['stunting', 'wasting']):
+        for group, group_df in pop.groupby(["stunting", "wasting"]):
             stunting_category, wasting_category = group
-            distribution = self.distributions[f"stunting_{stunting_category}_wasting_{wasting_category}"]
-            exposure = distribution.ppf(group_df['propensity'])
+            distribution = self.distributions[
+                f"stunting_{stunting_category}_wasting_{wasting_category}"
+            ]
+            exposure = distribution.ppf(group_df["propensity"])
             exposures.append(exposure)
         return pd.concat(exposures).sort_index()
