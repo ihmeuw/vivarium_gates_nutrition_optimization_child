c92db62a6d55105563394cc0b46852a17a495b67
Patrick Nast <pnast@uw.edu>
2023-09-12 14:09:30 -0700

    have <6mo wasting RR

--- src/vivarium_gates_nutrition_optimization_child/data/loader.py
+++ src/vivarium_gates_nutrition_optimization_child/data/loader.py
@@ -503,11 +503,6 @@ def load_gbd_2021_rr(key: str, location: str) -> pd.DataFrame:
         # Remove neonatal relative risks
         neonatal_age_ends = data.index.get_level_values("age_end").unique()[:2]
         data.loc[data.index.get_level_values("age_end").isin(neonatal_age_ends)] = 1.0
-    elif key == data_keys.WASTING.RELATIVE_RISK:
-        # Remove relative risks for simulants under 6 months
-        data.loc[
-            data.index.get_level_values("age_end") <= data_values.WASTING.DYNAMIC_START_AGE
-        ] = 1.0
     return data
 
 
