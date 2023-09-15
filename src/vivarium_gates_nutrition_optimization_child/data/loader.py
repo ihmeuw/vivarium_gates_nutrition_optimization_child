413d57a59baa0af8e0bd1a2ff907abeb6815bf6d
hussain-jafari <hussainsjafari@gmail.com>
2023-09-15 13:36:23 -0500

    fix expand_data

--- src/vivarium_gates_nutrition_optimization_child/data/loader.py
+++ src/vivarium_gates_nutrition_optimization_child/data/loader.py
@@ -312,10 +312,9 @@ def load_wasting_transition_rates(key: str, location: str) -> pd.DataFrame:
 
 
 def expand_data(data: pd.DataFrame, column_name: str, column_values: List) -> pd.DataFrame:
-    '''For each column value, create a copy of data with a new column with this value. Concat these copies.'''
-    join_index = pd.Index([1]*len(column_values))
-    new_values = pd.DataFrame({column_name: column_values}).set_index(join_index)
-    data = data.set_index(join_index).join(new_values)
+    '''Equivalent to: For each column value, create a copy of data with a new column with this value. Concat these copies.'''
+    new_values = pd.DataFrame({column_name: column_values}).set_index(pd.Index([1]*len(column_values)))
+    data = data.set_index(pd.Index([1]*len(data))).join(new_values)
     return data
 
 
