93f42e311a803877f08519c865c46f01933497c2
Hussain Jafari <hussainsjafari@gmail.com>
2023-11-06 11:14:24 -0600

    bugfix (#93)
        Category: bugfix
        JIRA issue: MIC-4629
    
    Changes and notes
    The old code was a placeholder while testing (and actually didn't work properly either). Properly make sure no simulants under 6 months are on treatment.
    
    Verification and Testing
    Made artifact.

--- src/vivarium_gates_nutrition_optimization_child/components/wasting.py
+++ src/vivarium_gates_nutrition_optimization_child/components/wasting.py
@@ -93,12 +93,6 @@ class WastingTreatment(Risk):
         is_mam_component = self.risk.name == "moderate_acute_malnutrition_treatment"
         coverage_to_exposure_map = {"none": "cat1", "full": "cat2"}
 
-        # simulants under 6 months should not be on treatment
-        if len(index) > 0:
-            # all simulants are the same age so just check the first simulant
-            if self.population_view.get(pd.Index([0]))["age"].squeeze() < 0.5:
-                return pd.Series("cat1", index=index)
-
         if is_mam_component:
             mam_coverage = self.scenario.mam_tx_coverage
             if mam_coverage == "baseline":  # return standard exposure if baseline
