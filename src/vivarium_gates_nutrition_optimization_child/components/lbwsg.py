f47f1d8c4620661b3f792e5b99fc608266691254
hussain-jafari <hussainsjafari@gmail.com>
2023-11-17 16:36:45 -0800

    copied from cvd

--- src/vivarium_gates_nutrition_optimization_child/components/lbwsg.py
+++ src/vivarium_gates_nutrition_optimization_child/components/lbwsg.py
@@ -206,3 +206,51 @@ class LBWSGPAFCalculationExposure(LBWSGRisk):
             exposure_values.loc[subset_index] = lbwsg_exposures[axis].values
 
         return exposure_values
+
+
+class LBWSGPAFObserver:
+    def __init__(self, risk: str, target: str):
+        self.configuration_defaults = self.get_configuration_defaults()
+
+    def __repr__(self):
+        return f"PAFObserver({self.risk}, {self.target})"
+
+    @property
+    def name(self):
+        return f"paf_observer.{self.risk}.{self.target}"
+
+    # noinspection PyAttributeOutsideInit
+    def setup(self, builder: Builder) -> None:
+        self.risk_effect = builder.components.get_component(
+            f"paf_calculation_risk_effect.{self.risk}.{self.target}"
+        )
+
+        config = builder.configuration.stratification[f"{self.risk.name}_paf"]
+
+        builder.results.register_observation(
+            name=f"calculated_paf_{self.risk}_on_{self.target}",
+            pop_filter='alive == "alive"',
+            aggregator=self.calculate_paf,
+            requires_columns=["alive"],
+            additional_stratifications=config.include,
+            excluded_stratifications=config.exclude,
+            when="time_step__prepare",
+        )
+
+    def calculate_paf(self, x: pd.DataFrame) -> float:
+        relative_risk = self.risk_effect.target_modifier(x.index, pd.Series(1, index=x.index))
+        mean_rr = relative_risk.mean()
+        paf = (mean_rr - 1) / mean_rr
+
+        return paf
+
+    def get_configuration_defaults(self) -> Dict[str, Dict]:
+        return {
+            "stratification": {
+                f"{self.risk.name}_paf_on_{self.target.name}": PAFObserver.configuration_defaults[
+                    "stratification"
+                ][
+                    "paf"
+                ]
+            }
+        }
