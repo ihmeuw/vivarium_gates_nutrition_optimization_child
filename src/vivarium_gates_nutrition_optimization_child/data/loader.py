bac5611bce45a7d695f5234026877a6fed27e722
Hussain Jafari <hjafari@uw.edu>
2023-12-14 09:41:01 -0800

    typos

--- src/vivarium_gates_nutrition_optimization_child/data/loader.py
+++ src/vivarium_gates_nutrition_optimization_child/data/loader.py
@@ -792,8 +792,8 @@ def load_gbd_2021_exposure(key: str, location: str) -> pd.DataFrame:
         new_cat2_rows[metadata.ARTIFACT_COLUMNS] = cat2_rows[metadata.ARTIFACT_COLUMNS] * probabilities[metadata.ARTIFACT_COLUMNS]
         cat25_rows[metadata.ARTIFACT_COLUMNS] = cat2_rows[metadata.ARTIFACT_COLUMNS] * (1 - probabilities[
             metadata.ARTIFACT_COLUMNS])
-        cat2_rows = cat2_rows.set_index(metadata.ARTIFACT_INDEX_COLUMNS + ['parameter']).sort_index()
-        cat25_rows = cat2_rows.set_index(metadata.ARTIFACT_INDEX_COLUMNS + ['parameter']).sort_index()
+        new_cat2_rows = new_cat2_rows.set_index(metadata.ARTIFACT_INDEX_COLUMNS + ['parameter']).sort_index()
+        cat25_rows = cat25_rows.set_index(metadata.ARTIFACT_INDEX_COLUMNS + ['parameter']).sort_index()
         breakpoint()
         data = pd.concat([rows_to_keep, new_cat2_rows, cat25_rows]).sort_index()
     return data
