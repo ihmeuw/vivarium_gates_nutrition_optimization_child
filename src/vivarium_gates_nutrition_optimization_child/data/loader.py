81ea41777340314b7f63bd46bab4681f99a324c1
Sylvia Lutze <lutzes@long-slurm-sarchive-p0041.cluster.ihme.washington.edu>
2024-05-29 14:02:50 -0700

    Subnational SQ-LNS effect data, adding subnational stratifiction, changing model specs

--- src/vivarium_gates_nutrition_optimization_child/data/loader.py
+++ src/vivarium_gates_nutrition_optimization_child/data/loader.py
@@ -92,7 +92,7 @@ NATIONAL_LEVEL_DATA_KEYS = [
     data_keys.LBWSG.EXPOSURE,
     data_keys.LBWSG.RELATIVE_RISK,
     data_keys.LBWSG.RELATIVE_RISK_INTERPOLATOR,
-    data_keys.LBWSG.PAF,
+    #data_keys.LBWSG.PAF,
     data_keys.AFFECTED_UNMODELED_CAUSES.URI_CSMR,
     data_keys.AFFECTED_UNMODELED_CAUSES.OTITIS_MEDIA_CSMR,
     data_keys.AFFECTED_UNMODELED_CAUSES.MENINGITIS_CSMR,
@@ -238,7 +238,7 @@ def get_data(
         data_keys.LBWSG.EXPOSURE: load_lbwsg_exposure,  ## Still 2019 age bins, but doesn't have effect past NN
         data_keys.LBWSG.RELATIVE_RISK: load_lbwsg_rr,  ## Still 2019 age bins, but doesn't have effect past NN
         data_keys.LBWSG.RELATIVE_RISK_INTERPOLATOR: load_lbwsg_interpolated_rr,  ## Still 2019 age bins, but doesn't have effect past NN
-        data_keys.LBWSG.PAF: load_lbwsg_paf,  ## Still 2019 age bins, but doesn't have effect past NN
+        #data_keys.LBWSG.PAF: load_lbwsg_paf,  ## Still 2019 age bins, but doesn't have effect past NN
         data_keys.AFFECTED_UNMODELED_CAUSES.URI_CSMR: load_standard_data,
         data_keys.AFFECTED_UNMODELED_CAUSES.OTITIS_MEDIA_CSMR: load_standard_data,
         data_keys.AFFECTED_UNMODELED_CAUSES.MENINGITIS_CSMR: load_standard_data,
@@ -1819,6 +1819,10 @@ def load_sqlns_risk_ratios(key: str, location: Union[str, List[int]]) -> pd.Data
 
     # generate draws using distribution parameters for each row
     risk_ratios = pd.read_csv(paths.SQLNS_RISK_RATIOS)
+    national_location_id = get_national_location_id(location[0])
+    risk_ratios = risk_ratios.query("national_id==@national_location_id").drop(
+    ["national_id", "location_id", "Unnamed: 0"], axis=1
+        ).reset_index()
     distributions = get_lognorm_from_quantiles(
         risk_ratios["median"], risk_ratios["lower"], risk_ratios["upper"]
     )
@@ -1827,7 +1831,7 @@ def load_sqlns_risk_ratios(key: str, location: Union[str, List[int]]) -> pd.Data
     )
 
     # reshape
-    index_cols = ["age_start", "age_end", "affected_outcome"]
+    index_cols = ["location", "age_start", "age_end", "affected_outcome"]
     draw_columns = pd.DataFrame(draw for draw in draws).T
     draw_columns.columns = metadata.ARTIFACT_COLUMNS
     data = pd.concat([risk_ratios[index_cols], draw_columns], axis=1).set_index(index_cols)
