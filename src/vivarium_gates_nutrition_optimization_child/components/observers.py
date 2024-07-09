062a1ac095f3ceea406f3c80ab22d1a408b80f0c
Steve Bachmeier <sbachmei@uw.edu>
2024-07-09 14:12:50 -0700

    update to new observers

--- src/vivarium_gates_nutrition_optimization_child/components/observers.py
+++ src/vivarium_gates_nutrition_optimization_child/components/observers.py
@@ -1,20 +1,13 @@
-from collections import Counter
-from typing import Dict, List, Optional
+from typing import Any, Dict
 
 import pandas as pd
-from vivarium import Component
 from vivarium.framework.engine import Builder
-from vivarium.framework.event import Event
-from vivarium.framework.population import PopulationView
-from vivarium_public_health.disease.transition import TransitionString
-from vivarium_public_health.metrics.disability import (
-    DisabilityObserver as DisabilityObserver_,
-)
-from vivarium_public_health.metrics.disease import DiseaseObserver
-from vivarium_public_health.metrics.mortality import (
+from vivarium_public_health.disease import DiseaseState
+from vivarium_public_health.results.disease import DiseaseObserver
+from vivarium_public_health.results.mortality import (
     MortalityObserver as MortalityObserver_,
 )
-from vivarium_public_health.metrics.stratification import (
+from vivarium_public_health.results.stratification import (
     ResultsStratifier as ResultsStratifier_,
 )
 
@@ -61,39 +54,39 @@ class ResultsStratifier(ResultsStratifier_):
         """Register each desired stratification with calls to _setup_stratification"""
         super().register_stratifications(builder)
 
-        builder.results.register_stratification(
-            "wasting_state",
-            [category.value for category in data_keys.ChildWastingCategories],
-            self.child_wasting_stratification_mapper,
-            is_vectorized=True,
-            requires_values=["child_wasting.exposure"],
-        )
-        builder.results.register_stratification(
-            "stunting_state",
-            [category.value for category in data_keys.CGFCategories],
-            self.child_growth_risk_factor_stratification_mapper,
-            is_vectorized=True,
-            requires_values=["child_stunting.exposure"],
-        )
-        builder.results.register_stratification(
-            "underweight_state",
-            [category.value for category in data_keys.CGFCategories],
-            self.child_growth_risk_factor_stratification_mapper,
-            is_vectorized=True,
-            requires_values=["child_underweight.exposure"],
-        )
-        builder.results.register_stratification(
-            "maternal_supplementation",
-            results.MATERNAL_SUPPLEMENTATION_TYPES,
-            is_vectorized=True,
-            requires_columns=["maternal_supplementation_exposure"],
-        )
-        builder.results.register_stratification(
-            "bmi_anemia",
-            ["cat4", "cat3", "cat2", "cat1"],
-            is_vectorized=True,
-            requires_columns=["maternal_bmi_anemia_exposure"],
-        )
+        # builder.results.register_stratification(
+        #     "wasting_state",
+        #     [category.value for category in data_keys.ChildWastingCategories],
+        #     self.child_wasting_stratification_mapper,
+        #     is_vectorized=True,
+        #     requires_values=["child_wasting.exposure"],
+        # )
+        # builder.results.register_stratification(
+        #     "stunting_state",
+        #     [category.value for category in data_keys.CGFCategories],
+        #     self.child_growth_risk_factor_stratification_mapper,
+        #     is_vectorized=True,
+        #     requires_values=["child_stunting.exposure"],
+        # )
+        # builder.results.register_stratification(
+        #     "underweight_state",
+        #     [category.value for category in data_keys.CGFCategories],
+        #     self.child_growth_risk_factor_stratification_mapper,
+        #     is_vectorized=True,
+        #     requires_values=["child_underweight.exposure"],
+        # )
+        # builder.results.register_stratification(
+        #     "maternal_supplementation",
+        #     results.MATERNAL_SUPPLEMENTATION_TYPES,
+        #     is_vectorized=True,
+        #     requires_columns=["maternal_supplementation_exposure"],
+        # )
+        # builder.results.register_stratification(
+        #     "bmi_anemia",
+        #     ["cat4", "cat3", "cat2", "cat1"],
+        #     is_vectorized=True,
+        #     requires_columns=["maternal_bmi_anemia_exposure"],
+        # )
         builder.results.register_stratification(
             "sam_treatment",
             ["covered", "uncovered"],
@@ -114,12 +107,12 @@ class ResultsStratifier(ResultsStratifier_):
             is_vectorized=True,
             requires_values=[data_values.SQ_LNS.COVERAGE_PIPELINE],
         )
-        builder.results.register_stratification(
-            "birth_weight_status",
-            ["low_birth_weight", "adequate_birth_weight"],
-            is_vectorized=True,
-            requires_columns=["birth_weight_status"],
-        )
+        # builder.results.register_stratification(
+        #     "birth_weight_status",
+        #     ["low_birth_weight", "adequate_birth_weight"],
+        #     is_vectorized=True,
+        #     requires_columns=["birth_weight_status"],
+        # )
         location = builder.data.load("population.location")
         builder.results.register_stratification(
             "subnational",
@@ -133,26 +126,26 @@ class ResultsStratifier(ResultsStratifier_):
     ###########################
 
     # noinspection PyMethodMayBeStatic
-    def child_growth_risk_factor_stratification_mapper(self, pop: pd.DataFrame) -> pd.Series:
-        # applicable to stunting and wasting
-        mapper = {
-            "cat4": data_keys.CGFCategories.UNEXPOSED.value,
-            "cat3": data_keys.CGFCategories.MILD.value,
-            "cat2": data_keys.CGFCategories.MODERATE.value,
-            "cat1": data_keys.CGFCategories.SEVERE.value,
-        }
-        return pop.squeeze(axis=1).map(mapper)
+    # def child_growth_risk_factor_stratification_mapper(self, pop: pd.DataFrame) -> pd.Series:
+    #     # applicable to stunting and wasting
+    #     mapper = {
+    #         "cat4": data_keys.CGFCategories.UNEXPOSED.value,
+    #         "cat3": data_keys.CGFCategories.MILD.value,
+    #         "cat2": data_keys.CGFCategories.MODERATE.value,
+    #         "cat1": data_keys.CGFCategories.SEVERE.value,
+    #     }
+    #     return pop.squeeze(axis=1).map(mapper)
 
-    def child_wasting_stratification_mapper(self, pop: pd.DataFrame) -> pd.Series:
-        # applicable to stunting and wasting
-        mapper = {
-            "cat4": data_keys.ChildWastingCategories.UNEXPOSED.value,
-            "cat3": data_keys.ChildWastingCategories.MILD.value,
-            "cat2.5": data_keys.ChildWastingCategories.BETTER_MODERATE.value,
-            "cat2": data_keys.ChildWastingCategories.WORSE_MODERATE.value,
-            "cat1": data_keys.ChildWastingCategories.SEVERE.value,
-        }
-        return pop.squeeze(axis=1).map(mapper)
+    # def child_wasting_stratification_mapper(self, pop: pd.DataFrame) -> pd.Series:
+    #     # applicable to stunting and wasting
+    #     mapper = {
+    #         "cat4": data_keys.ChildWastingCategories.UNEXPOSED.value,
+    #         "cat3": data_keys.ChildWastingCategories.MILD.value,
+    #         "cat2.5": data_keys.ChildWastingCategories.BETTER_MODERATE.value,
+    #         "cat2": data_keys.ChildWastingCategories.WORSE_MODERATE.value,
+    #         "cat1": data_keys.ChildWastingCategories.SEVERE.value,
+    #     }
+    #     return pop.squeeze(axis=1).map(mapper)
 
     def map_wasting_treatment(self, pop: pd.DataFrame) -> pd.Series:
         # Both SAM and MAM treatments
@@ -179,120 +172,131 @@ class ResultsStratifier(ResultsStratifier_):
         bins = self.age_bins["age_start"].to_list() + [self.age_bins["age_end"].iloc[-1]]
         labels = self.age_bins["age_group_name"].to_list()
         # need to include lowest to map people who are exactly 0
+        pop = pop.copy()
+        pop.loc[pop.age > 5, "age"] = 5
         age_group = pd.cut(
             pop.squeeze(axis=1), bins, labels=labels, include_lowest=True
         ).rename("age_group")
         return age_group
 
 
-class BirthObserver(Component):
-    CONFIGURATION_DEFAULTS = {
-        "stratification": {
-            "birth": {
-                "exclude": [],
-                "include": [],
-            }
-        }
-    }
+# class BirthObserver(Component):
+#     CONFIGURATION_DEFAULTS = {
+#         "stratification": {
+#             "birth": {
+#                 "exclude": [],
+#                 "include": [],
+#             }
+#         }
+#     }
 
-    def __init__(self):
-        super().__init__()
-        self.birth_weight_column_name = "birth_weight_exposure"
-        self.gestational_age_column_name = "gestational_age_exposure"
-        self.low_birth_weight_limit = 2500  # grams
+#     def __init__(self):
+#         super().__init__()
+#         self.birth_weight_column_name = "birth_weight_exposure"
+#         self.gestational_age_column_name = "gestational_age_exposure"
+#         self.low_birth_weight_limit = 2500  # grams
 
-    @property
-    def columns_required(self) -> Optional[List[str]]:
-        return [
-            "entrance_time",
-            self.birth_weight_column_name,
-            self.gestational_age_column_name,
-        ]
+#     @property
+#     def columns_required(self) -> Optional[List[str]]:
+#         return [
+#             "entrance_time",
+#             self.birth_weight_column_name,
+#             self.gestational_age_column_name,
+#         ]
 
-    #################
-    # Setup methods #
-    #################
+#     #################
+#     # Setup methods #
+#     #################
 
-    # noinspection PyAttributeOutsideInit
-    def setup(self, builder: Builder) -> None:
-        self.clock = builder.time.clock()
-        self.config = builder.configuration.stratification.birth
+#     # noinspection PyAttributeOutsideInit
+#     def setup(self, builder: Builder) -> None:
+#         self.clock = builder.time.clock()
+#         self.config = builder.configuration.stratification.birth
 
-        builder.results.register_observation(
-            name=f"live_births",
-            pop_filter="alive=='alive'",
-            aggregator=self.count_live_births,
-            requires_columns=["entrance_time"],
-            additional_stratifications=self.config.include,
-            excluded_stratifications=self.config.exclude,
-            when="collect_metrics",
-        )
-        builder.results.register_observation(
-            name=f"birth_weight_sum",
-            pop_filter="alive=='alive'",
-            aggregator=self.sum_birth_weights,
-            requires_columns=["entrance_time", self.birth_weight_column_name],
-            additional_stratifications=self.config.include,
-            excluded_stratifications=self.config.exclude,
-            when="collect_metrics",
-        )
-        builder.results.register_observation(
-            name=f"gestational_age_sum",
-            pop_filter="alive=='alive'",
-            aggregator=self.sum_gestational_ages,
-            requires_columns=["entrance_time", self.gestational_age_column_name],
-            additional_stratifications=self.config.include,
-            excluded_stratifications=self.config.exclude,
-            when="collect_metrics",
-        )
-        builder.results.register_observation(
-            name=f"low_weight_births",
-            pop_filter="alive=='alive'",
-            aggregator=self.count_low_weight_births,
-            requires_columns=["entrance_time", self.birth_weight_column_name],
-            additional_stratifications=self.config.include,
-            excluded_stratifications=self.config.exclude,
-            when="collect_metrics",
-        )
+#         builder.results.register_observation(
+#             name=f"live_births",
+#             pop_filter="alive=='alive'",
+#             aggregator=self.count_live_births,
+#             requires_columns=["entrance_time"],
+#             additional_stratifications=self.config.include,
+#             excluded_stratifications=self.config.exclude,
+#             when="collect_metrics",
+#         )
+#         builder.results.register_observation(
+#             name=f"birth_weight_sum",
+#             pop_filter="alive=='alive'",
+#             aggregator=self.sum_birth_weights,
+#             requires_columns=["entrance_time", self.birth_weight_column_name],
+#             additional_stratifications=self.config.include,
+#             excluded_stratifications=self.config.exclude,
+#             when="collect_metrics",
+#         )
+#         builder.results.register_observation(
+#             name=f"gestational_age_sum",
+#             pop_filter="alive=='alive'",
+#             aggregator=self.sum_gestational_ages,
+#             requires_columns=["entrance_time", self.gestational_age_column_name],
+#             additional_stratifications=self.config.include,
+#             excluded_stratifications=self.config.exclude,
+#             when="collect_metrics",
+#         )
+#         builder.results.register_observation(
+#             name=f"low_weight_births",
+#             pop_filter="alive=='alive'",
+#             aggregator=self.count_low_weight_births,
+#             requires_columns=["entrance_time", self.birth_weight_column_name],
+#             additional_stratifications=self.config.include,
+#             excluded_stratifications=self.config.exclude,
+#             when="collect_metrics",
+#         )
 
-    ########################
-    # Event-driven methods #
-    ########################
-    def count_live_births(self, x: pd.DataFrame) -> float:
-        born_this_step = x["entrance_time"] == self.clock()
-        return sum(born_this_step)
+#     ########################
+#     # Event-driven methods #
+#     ########################
+#     def count_live_births(self, x: pd.DataFrame) -> float:
+#         born_this_step = x["entrance_time"] == self.clock()
+#         return sum(born_this_step)
 
-    def sum_birth_weights(self, x: pd.DataFrame) -> float:
-        born_this_step = x["entrance_time"] == self.clock()
-        return x.loc[born_this_step, self.birth_weight_column_name].sum()
+#     def sum_birth_weights(self, x: pd.DataFrame) -> float:
+#         born_this_step = x["entrance_time"] == self.clock()
+#         return x.loc[born_this_step, self.birth_weight_column_name].sum()
 
-    def sum_gestational_ages(self, x: pd.DataFrame) -> float:
-        born_this_step = x["entrance_time"] == self.clock()
-        return x.loc[born_this_step, self.gestational_age_column_name].sum()
+#     def sum_gestational_ages(self, x: pd.DataFrame) -> float:
+#         born_this_step = x["entrance_time"] == self.clock()
+#         return x.loc[born_this_step, self.gestational_age_column_name].sum()
 
-    def count_low_weight_births(self, x: pd.DataFrame) -> float:
-        born_this_step = x["entrance_time"] == self.clock()
-        has_low_birth_weight = (
-            x.loc[born_this_step, self.birth_weight_column_name] < self.low_birth_weight_limit
-        )
-        return sum(has_low_birth_weight)
+#     def count_low_weight_births(self, x: pd.DataFrame) -> float:
+#         born_this_step = x["entrance_time"] == self.clock()
+#         has_low_birth_weight = (
+#             x.loc[born_this_step, self.birth_weight_column_name] < self.low_birth_weight_limit
+#         )
+#         return sum(has_low_birth_weight)
 
 
 class MortalityObserver(MortalityObserver_):
-    """This is a class to make component ordering work in the model spec."""
-
-    pass
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
+        stillborn = DiseaseState("stillborn")
+        stillborn.set_model("stillborn")
+        self.causes_of_death += [stillborn]
 
 
 class ChildWastingObserver(DiseaseObserver):
     def __init__(self):
         super().__init__("child_wasting")
-        self.disease = self.risk = "child_wasting"
-        self.exposure_pipeline_name = f"{self.risk}.exposure"
+        self.risk = self.disease
+        self.exposure_pipeline_name = f"{self.disease}.exposure"
 
     @property
-    def columns_required(self) -> Optional[List[str]]:
-        return [self.disease, "sex", "age"]
+    def configuration_defaults(self) -> Dict[str, Any]:
+        return {
+            "stratification": {
+                self.disease: {
+                    "exclude": [],
+                    "include": [],
+                }
+            }
+        }
 
     #################
     # Setup methods #
@@ -302,75 +306,9 @@ class ChildWastingObserver(DiseaseObserver):
     def setup(self, builder: Builder) -> None:
         self.step_size = builder.time.step_size()
         self.config = builder.configuration.stratification[self.disease]
-        self.categories = builder.data.load(f"risk_factor.{self.risk}.categories")
-
-        disease_model = builder.components.get_component(
+        self.disease_model = builder.components.get_component(
             f"child_wasting_model.{self.disease}"
         )
-
-        # not needed for final runs
-        for category in self.categories:
-            builder.results.register_observation(
-                name=f"{self.risk}_{category}_person_time",
-                pop_filter=f'alive == "alive" and `{self.exposure_pipeline_name}`=="{category}" and tracked==True',
-                aggregator=self.aggregate_state_person_time,
-                requires_columns=["alive"],
-                requires_values=[self.exposure_pipeline_name],
-                additional_stratifications=self.config.include,
-                excluded_stratifications=self.config.exclude,
-                when="time_step__prepare",
-            )
-
-        for transition in disease_model.transition_names:
-            filter_string = (
-                f'{self.previous_state_column_name} == "{transition.from_state}" '
-                f'and {self.disease} == "{transition.to_state}" '
-                f"and tracked==True"
-            )
-            builder.results.register_observation(
-                name=f"{transition}_event_count",
-                pop_filter=filter_string,
-                requires_columns=[self.previous_state_column_name, self.disease],
-                additional_stratifications=self.config.include,
-                excluded_stratifications=self.config.exclude,
-                when="collect_metrics",
-            )
-
-
-class MortalityHazardRateObserver(Component):
-    CONFIGURATION_DEFAULTS = {
-        "stratification": {
-            "mortality_hazard_rate": {
-                "exclude": [],
-                "include": [],
-            }
-        }
-    }
-
-    def __init__(self):
-        super().__init__()
-        self.mortality_rate_pipeline_name = "mortality_rate"
-
-    #################
-    # Setup methods #
-    #################
-
-    # noinspection PyAttributeOutsideInit
-    def setup(self, builder: Builder):
-        self.config = builder.configuration.stratification.mortality_hazard_rate
-        self.mortality_rates = builder.value.get_value(self.mortality_rate_pipeline_name)
-
-        builder.results.register_observation(
-            name=f"mortality_hazard_rate_first_moment",
-            pop_filter="alive=='alive'",
-            aggregator=self.calculate_mortality_hazard_rate,
-            requires_columns=["alive"],
-            additional_stratifications=self.config.include,
-            excluded_stratifications=self.config.exclude,
-            when="collect_metrics",
-        )
-
-    def calculate_mortality_hazard_rate(self, x: pd.DataFrame) -> float:
-        # sum mortality rates across all causes
-        summed_mortality_rates = self.mortality_rates(x.index).sum()
-        return sum(summed_mortality_rates)
+        self.entity_type = self.disease_model.cause_type
+        self.entity = self.disease_model.cause
+        self.transition_stratification_name = f"transition_{self.disease}"
