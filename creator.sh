d89ed995f4ba16b3d739cc6b5dbb7737a0d0d411
albrja <albrja@uw.edu>
2024-07-12 13:42:56 -0700

    Update script

--- creator.sh
+++ creator.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
-# This script creates a conda environment from a requirements.txt file from Github.
-# Conda environments should be created with the simulation science service user and 
-# the credientials can be found at /mnt/team/simulation_science/priv/credentials/svc-simsci
+# This script creates a conda environment from a requirements.txt file from Github model repository.
+# This sript should be run from the home directory of the repo. The script takes two arguments:
+# The first argument is the environment name
+# The second argument is the environment type. This will either be "simulation" or "artifact" e.g. 
+# what the user will be using the environment to develop.
 
-# Three arguments must be provided to the script:
-#   - The first argument is the name of the conda environment
-#   - The second argument is the repo name
-#   - The third argument is the name of the branch to fetch requirements.txt from for
-#       repo specified in argument two.
+# Run this script by: source creator.sh <ENVIRONMENT_NAME> <ENVIRONMENT_TYPE>
 
 env_name=$1
-repo_name=$2
-branch_name=$3
+env_type=$2
+
+# Determine which requirements.txt to install from
+if [ $env_type == 'simulation' ]; then
+  echo "Installing requirements for simulation environment..."
+  install_file="requirements.txt"
+elif [ $env_type == 'artifact' ]; then
+  echo "Installing requirements for artifact environment"
+  install_file="artifact_requirements.txt"
+else
+  echo "Invalid environment type. Valid argument types are simulation (default) and artifact."
+  sleep 
+fi
+
 
 # Create conda environment
-conda create -p /mnt/team/simulation_science/pub/envs/$env_name python=3.11 -y
+#conda create -p /mnt/team/simulation_science/pub/envs/$env_name python=3.11 -y
+conda create -n $env_name python=3.11 -y
 
 # Activate new environment
-conda activate /mnt/team/simulation_science/pub/envs/$env_name
+# conda activate /mnt/team/simulation_science/pub/envs/$env_name
+conda activate $env_name
 
 # Install requirements via Github
-pip install -r https://raw.githubusercontent.com/ihmeuw/$repo_name/$branch_name/requirements.txt 
+# pip install -r https://raw.githubusercontent.com/ihmeuw/$repo_name/$branch_name/requirements.txt 
+pip install -r https://raw.githubusercontent.com/ihmeuw/vivarium_gates_nutrition_optimization_child/mic-5146/create-conda-from-commits/$install_file 
 
-# Install redis for sims
-conda install redis -y
-    
+# Editable install of repo
+pip install -e .[dev]
+
+# Install redis for simulation environments
+if env_type=="simulation"
+then
+  conda install redis -y
+fi
