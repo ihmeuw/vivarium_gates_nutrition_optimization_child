d06c75178d590baf228574ed201569f380c0dbc1
albrja <37345113+albrja@users.noreply.github.com>
2024-07-22 11:21:56 -0700

    Update branch name (#152)
    * Update branch file
    
    * Add comentt

--- creator.sh
+++ creator.sh
@@ -31,7 +31,8 @@ conda create -n $env_name python=3.11 -y
 conda activate $env_name
 
 # Install requirements via Github
-pip install -r https://raw.githubusercontent.com/ihmeuw/vivarium_gates_nutrition_optimization_child/mic-5146/create-conda-from-commits/$install_file 
+# NOTE: update branch name if you update requirements.txt in a branch
+pip install -r https://raw.githubusercontent.com/ihmeuw/vivarium_gates_nutrition_optimization_child/main/$install_file 
 
 # Editable install of repo
 pip install -e .[dev]
