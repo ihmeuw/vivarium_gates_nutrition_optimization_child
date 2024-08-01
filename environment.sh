810a298871f5578e6a9b4fa08854f8ca61e67284
albrja <albrja@uw.edu>
2024-08-01 15:56:42 -0700

    Address PR comments

--- environment.sh
+++ environment.sh
@@ -60,13 +60,14 @@ requirements_modification_time="$(date -r $install_file '+%Y-%m-%d %H:%M:%S')"
 create_env=$(conda info --envs | grep $env_name | head -n 1)
 if [[ $create_env == '' ]]; then
   # No environment exists with this name
+  echo "Environment $env_name does not exist."
   create_env="yes"
 else
   create_env="no"
 fi
 
+# Check if existing environment needs to be recreated
 if [ $create_env == 'no' ]; then
-  # Check if existing environment needs to be updated
   echo "Existing environment found for $env_name."
   one_week_ago=$(date -d "7 days ago" '+%Y-%m-%d %H:%M:%S')
   creation_time="$(head -n1 /home/$username/miniconda3/envs/$env_name/conda-meta/history)"
@@ -75,38 +76,18 @@ if [ $create_env == 'no' ]; then
     echo "Environment is older than one week old. Deleting and remaking environment...."
     conda remove -n $env_name --all -y
     create_env="yes"
+  else
+    echo "Environment created within the last week."
   fi
-fi
-
-if [ $create_env == 'yes' ]; then
-  # Create new environment
-  echo "Environment $env_name does not exist. Creating new environment $env_name..."
-  # Create conda environment
-  conda create -n $env_name python=3.11 -y
-fi
-
-# Activate environment
-conda activate $env_name
-if [ $create_env == 'yes' ]; then
-  # Install json parser
-  conda install jq -y
-  update_env='yes'
-else
-  update_env='no'
-fi
-
-# At this point we have either a new environment or an environment that is less than a week old.
-# We need to check if we need to reinstall packages
-if [ $update_env == 'no' ]; then
   # Check if environment was build before last modification to requirements file
   if [[ $creation_time < $requirements_modification_time ]]; then
     echo "Requirements file last modified after environment was created. Reinstalling packages..."
-    update_env="yes"
+    create_env="yes"
+  else
+    echo "Environment created after most recent change to requirements file."
   fi
-fi
-
-# Check if there has been an update to vivarium packages since last modification to requirements file
-if [ $update_env == 'no' ]; then
+  # Check if there has been an update to vivarium packages since last modification to requirements file
+  # or more reccent than environment creation
   grep @ $install_file 
   # TODO: can we make this grep output a variable?
   while read -r line ; do
@@ -115,8 +96,8 @@ if [ $update_env == 'no' ]; then
     repo=${repo_info[0]}
     repo_branch=${repo_info[2]}
     last_commit_time=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/ihmeuw/$repo/commits?sha=$repo_branch | jq '.["0"].commit.committer.date')
-    if [[ $requirements_modification_time < $last_commit_time ]]; then
-      update_env="yes"
+    if [[ $requirements_modification_time < $last_commit_time ]] && [[ $creation_time > $last_commit_time ]]; then
+      create_env="yes"
       break
     fi
   # This way of writing/exiting while loop is a here string so the process runs
@@ -125,8 +106,24 @@ if [ $update_env == 'no' ]; then
   done <<< "$(echo "")"
 fi
 
+if [ $create_env == 'yes' ]; then
+  # Create new environment
+  echo "Creating new environment $env_name..."
+  # Create conda environment
+  conda create -n $env_name python=3.11 -y
+else
+  echo "Existing environment validated"
+fi
+
+# Activate environment
+conda activate $env_name
+if [ $create_env == 'yes' ]; then
+  # Install json parser
+  conda install jq -y
+fi
+
 # Install requirements via Github
-if [ $update_env == 'yes' ]; then
+if [ $create_env == 'yes' ]; then
   # NOTE: update branch name if you update requirements.txt in a branch
   echo "Installing packages for $env_type environment"
   pip install -r https://raw.githubusercontent.com/ihmeuw/vivarium_gates_nutrition_optimization_child/main/$install_file  
