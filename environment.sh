436127426e5807636c0a1c839dead7a535b566fb
albrja <albrja@uw.edu>
2024-08-27 11:35:17 -0700

    Fix bug in deactivate and looking for history

--- environment.sh
+++ environment.sh
@@ -68,7 +68,8 @@ else
   # Check if existing environment needs to be recreated
   echo "Existing environment found for $env_name."
   one_week_ago=$(date -d "7 days ago" '+%Y-%m-%d %H:%M:%S')
-  creation_time="$(head -n1 /home/$username/miniconda3/envs/$env_name/conda-meta/history)"
+  env_path=$(conda info --envs | grep -Po "$env_name\K.*" | sed "s: ::g" | tr -d "*")
+  creation_time="$(head -n1 $env_path/conda-meta/history)"
   requirements_modification_time="$(date -r $install_file '+%Y-%m-%d %H:%M:%S')"
   # Check if existing environment is older than a week or if environment was built 
   # before last modification to requirements file. If so, mark for recreation.
@@ -105,7 +106,7 @@ fi
 
 if [[ $create_env == 'yes' ]]; then
   if [[ $env_exists == 'yes' ]]; then
-    conda deactivate $env_name
+    conda deactivate
     conda remove -n $env_name --all -y
   fi
   # Create conda environment
