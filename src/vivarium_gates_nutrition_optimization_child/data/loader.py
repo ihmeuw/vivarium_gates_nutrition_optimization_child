09c333a8870b64e6c3f672b3dd004dcef061efbb
Patrick Nast <pnast@uw.edu>
2026-04-17 14:25:54 -0700

    fix sam treatment states

--- src/vivarium_gates_nutrition_optimization_child/data/loader.py
+++ src/vivarium_gates_nutrition_optimization_child/data/loader.py
@@ -1218,7 +1218,7 @@ def load_sam_treatment_rr(key: str, location: str) -> pd.DataFrame:
     rr_sam_treated_remission = sam_tx_efficacy / sam_tx_efficacy_tmrel
     rr_sam_treated_remission[
         "affected_entity"
-    ] = "severe_acute_malnutrition_to_mild_child_wasting"
+    ] = "uncomplicated_severe_acute_malnutrition_to_mild_child_wasting"
 
     # rr_r2 = r2 / r2_tmrel
     #       = (1 - sam_tx_efficacy) * (r2_ux) / (1 - sam_tx_efficacy_tmrel) * (r2_ux)
@@ -1229,10 +1229,10 @@ def load_sam_treatment_rr(key: str, location: str) -> pd.DataFrame:
     worse_mam_rows = rr_sam_untreated_remission.copy()
     better_mam_rows[
         "affected_entity"
-    ] = "severe_acute_malnutrition_to_better_moderate_acute_malnutrition"
+    ] = "uncomplicated_severe_acute_malnutrition_to_better_moderate_acute_malnutrition"
     worse_mam_rows[
         "affected_entity"
-    ] = "severe_acute_malnutrition_to_worse_moderate_acute_malnutrition"
+    ] = "uncomplicated_severe_acute_malnutrition_to_worse_moderate_acute_malnutrition"
     rr_sam_untreated_remission = pd.concat([better_mam_rows, worse_mam_rows])
 
     rr = pd.concat([rr_sam_treated_remission, rr_sam_untreated_remission])
