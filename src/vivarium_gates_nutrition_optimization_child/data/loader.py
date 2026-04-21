d905c3e32ca22ef19b8224e0737ddacea7d6183a
Patrick Nast <pnast@uw.edu>
2026-04-21 12:09:04 -0700

    fix name

--- src/vivarium_gates_nutrition_optimization_child/data/loader.py
+++ src/vivarium_gates_nutrition_optimization_child/data/loader.py
@@ -1442,7 +1442,7 @@ def load_mam_treatment_rr(key: str, location: str) -> pd.DataFrame:
 
     demography = get_data(data_keys.POPULATION.DEMOGRAPHY, location).reset_index()
     mam_tx_efficacy, mam_tx_efficacy_tmrel = utilities.get_treatment_efficacy(
-        demography, data_keys.WASTING.CAT2, location
+        demography, data_keys.WASTING.CAT20_WORSE, location
     )
     index = mam_tx_efficacy.index
 
